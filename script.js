ad0db14a7b64d751a6373ba595165cd8d24d1468
Phung Minh Tuan Anh <tuananhphungminh1212@gmail.com>
2023-03-20 14:51:37 +0700

    update board

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+let game = {}
+function getJSON() {
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    //         'X-RapidAPI-Host': 'realtor.p.rapidapi.com'
+    //     }
+    // };
 
-var size = 50;
-var gameBoard = document.getElementsByClassName("gameboard");
-var gameBoardHTML="<table>";
-for(var i = 0; i<size; i++){
-    gameBoardHTML+="<tr>"
-    for(var j = 0; j<size; j++){
-        gameBoardHTML+="<td></td>"
-    }
-    gameBoardHTML+="</tr>";
+    fetch('test.json')
+        .then(response => response.json())
+        .then(response => game = response)
+        .then(response => console.log(game))
+        .catch(err => console.error(err));
+
 }
-gameBoardHTML+="</table>";
-gameBoard[0].innerHTML=gameBoardHTML;
+
+setInterval(getJSON, 1000);
+setInterval(drawBoard, 1000);
+
+function drawBoard() {
+    var size = game.size;
+    console.log(size);
+    var gameBoard = document.getElementsByClassName("gameboard");
+    var gameBoardHTML = "<table>";
+    for (var i = 0; i < size; i++) {
+        gameBoardHTML += "<tr>"
+        for (var j = 0; j < size; j++) {
+            gameBoardHTML += "<td></td>"
+        }
+        gameBoardHTML += "</tr>";
+    }
+    gameBoardHTML += "</table>";
+    gameBoard[0].innerHTML = gameBoardHTML;
+}

b532076fc234edcaff30ba64b602c0f23c2d0350
Phung Minh Tuan Anh <tuananhphungminh1212@gmail.com>
2023-03-20 19:57:11 +0700

    update time and flag

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let game = {}
+let time1 = 100
+let time2 = 100
 function getJSON() {
     // const options = {
     //     method: 'GET',
@@ -17,6 +19,8 @@ function getJSON() {
 
 setInterval(getJSON, 1000);
 setInterval(drawBoard, 1000);
+setInterval(countdown, 1000);
+setInterval(renderTime, 1000)
 
 function drawBoard() {
     var size = game.size;
@@ -26,10 +30,33 @@ function drawBoard() {
     for (var i = 0; i < size; i++) {
         gameBoardHTML += "<tr>"
         for (var j = 0; j < size; j++) {
-            gameBoardHTML += "<td></td>"
+            
+            gameBoardHTML += "<td>" + game[i][j] +"</td>"
         }
         gameBoardHTML += "</tr>";
     }
     gameBoardHTML += "</table>";
     gameBoard[0].innerHTML = gameBoardHTML;
 }
+
+
+function countdown() {
+    if(game.turn === game.team1_id) {
+        document.getElementById('turn-flag-2').style.visibility = "hidden"
+        document.getElementById('turn-flag-1').style.visibility = "visible"
+        time1--;
+    }
+    else if(game.turn === game.team2_id) {
+        document.getElementById('turn-flag-2').style.visibility = "visible"
+        document.getElementById('turn-flag-1').style.visibility = "hidden"
+        time2--;
+    }
+}
+
+function renderTime() {
+    document.getElementById('player1-time').innerHTML = time1
+    document.getElementById('player2-time').innerHTML = time2
+    
+}
+
+

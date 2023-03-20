69d36c724ef41bfdfaf6d9288b107d89e5a17b99
dkhien <dkhien.work@gmail.com>
2023-03-20 14:49:13 +0700

    update css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-var size = 50;
+var size = 10;
 var gameBoard = document.getElementsByClassName("gameboard");
-var gameBoardHTML="<table>";
+var gameBoardHTML="<table cellspacing='0'>";
 for(var i = 0; i<size; i++){
     gameBoardHTML+="<tr>"
     for(var j = 0; j<size; j++){

e24df8352648bc520e38792390c06a57de5bcc50
Phung Minh Tuan Anh <tuananhphungminh1212@gmail.com>
2023-03-22 10:20:57 +0700

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component() {
+    render() {
+        return (
+            <div className="App">
+                <div class="scoreboard">
+                    <div class="score1-container">
+                        <div id="score1"></div>
+                        <div class="turn-flag" id="turn-flag-1">
+                            <img src="resources/flag.png" alt="" />
+                        </div>
+                    </div>
+                    <div id="match-info">
+                        <div id="match-id">Match ID: 123</div>
+                        <div class="players-container">
+                            <div class="player1">
+                                <div id="player1-info">
+                                    <div id="player1-id"></div>
+                                    <div id="player1-time"></div>
+                                </div>
+
+                                <div class="player1-avatar">
+                                    <img src="resources/player1avatar.png" alt="" />
+                                </div>
+                            </div>
+                            <div class="player2">
+                                <div id="player2-info">
+                                    <div id="player2-id"></div>
+                                    <div id="player2-time"></div>
+                                </div>
+
+                                <div class="player2-avatar">
+                                    <img src="resources/player2avatar.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="score2-container">
+                        <div id="score2"></div>
+                        <div class="turn-flag" id="turn-flag-2" >
+                            <img src="resources/flag.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div class="gameboard-container">
+                    <div class="gameboard">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
